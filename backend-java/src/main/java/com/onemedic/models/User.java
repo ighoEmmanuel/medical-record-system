74f846683b120bd687f031b422414e5bb7e85adc
ighoEmmanuel <ighoe571@gmail.com>
2025-04-03 12:14:23 +0100

    working on models

---
+++ backend-java/src/main/java/com/onemedic/models/User.java
@@ -0,0 +1,15 @@
+package com.onemedic.models;
+
+import java.util.Date;
+
+
+
+public abstract class  User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Date date0fBirth;
+    private Gender gender;
+}
